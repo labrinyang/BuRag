72f17301185e00c87e6452e4e50952ea3e689612
labrinyang <zhiweiy9125@gmail.com>
2025-03-19 19:15:33 +0800

    fix bug

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from qNewsAPI import router as qNewsAPI_router
 
 app = FastAPI()
 
-app.include_router(qRootdataAPI_router, prefix="/crypto ")
+app.include_router(qRootdataAPI_router, prefix="/crypto")
 app.include_router(qNewsAPI_router, prefix="/cryoto")
